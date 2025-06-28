14cc3be1e9e03dcace23be1f1000ed692803479d
aarav505 <aaravanand2008@yahoo.com>
2025-06-28 22:42:05 +0530

    Set API URL to https://dsfl-backend.onrender.com

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5001';
+export const API_BASE_URL = 'https://dsfl-backend.onrender.com/';
 
 export const API_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/login`,
